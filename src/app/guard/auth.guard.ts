34f8867b557bccf9aa05b7de9c9c013cdd4b3754
ljubomir-cakarevic <ljubomir.cakarevic@gmail.com>
2021-06-01 12:28:53 +0200

    Refactor auth guard

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -7,35 +7,20 @@ import { TokenStorageService } from '../_services/token-storage.service';
 
 @Injectable({
     providedIn: 'root'
-  })
+})
 export class AuthGuard implements CanActivate {
-    
+
     constructor(
-        private authService: AuthService, 
-        private router: Router, 
-        private tokenStorage: TokenStorageService) {}
+        private authService: AuthService,
+        private router: Router,
+        private tokenStorage: TokenStorageService) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-        /* //if user is logged in(token is present) returns true (can continue to the special route)
-        if(this.authService.loggedIn()) {
-            return true;
-            
-        //if user is not logged in, redirect to login route
-        }else {
-            this.router.navigate(['/login']);
-            return false;
-        } 
-        const user: any = this.tokenStorage.getUser();
-        if (user.roles === 'ROLE_ADMIN') {
-        this.router.navigate(['/']);
-        }
-        return true;
-        } */
         const user: any = this.tokenStorage.getUser();
         if (user.roles.includes('ROLE_USER' || 'ROLE_MODERATOR')) {
-        this.router.navigate(['/login']);
+            this.router.navigate(['/login']);
         }
         return true;
-  }
+    }
 
 }
